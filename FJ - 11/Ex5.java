6dd4d779b57f4ad38a554f129e790cfc9cb3d996
woneto <woliveiraneto1996@gmail.com>
2016-12-07 19:54:24 -0200

    Add files via upload

---
+++ FJ - 11/Ex5.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args){
+		int gastoJaneiro = 15000;
+		int gastoFevereiro = 23000;
+		int gastoMarco = 17000;
+		int gastoTotal = gastoJaneiro + gastoFevereiro + gastoMarco;
+		System.out.println(gastoTotal);
+		System.out.println(gastoTotal / 3); //Média 
+	}
+}
